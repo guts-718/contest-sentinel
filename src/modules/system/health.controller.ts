73be22b8e4b5d3c4a39d0e1dd0b0f106ecc1c489
guts-718 <rahul.20223195@mnnit.ac.in>
2026-02-18 18:19:57 +0530

    added express server setup

--- src/modules/system/health.controller.ts
+++ src/modules/system/health.controller.ts
@@ -4,6 +4,7 @@ import { isDBConnected } from "../../core/db";
 const startTime = Date.now();
 
 export function healthHandler(req: Request, res: Response) {
+  console.log("Request: ",req);
   res.json({
     status: "ok",
     uptimeSeconds: Math.floor((Date.now() - startTime) / 1000),
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { config } from "./core/env";
+import { log } from "./core/logger";
+import { healthHandler } from "./modules/system/health.controller";
+
+export function startServer() {
+  const app = express();
+
+  app.use(express.json());
+
+  // routes
+  app.get("/health", healthHandler);
+
+  app.listen(config.PORT, () => {
+    log.api(`Server running on port ${config.PORT}`);
+  });
+}
+
+startServer();
