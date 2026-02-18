6e2ced0dbd94cdd50bd1bc862e2769a3289b6bbc
guts-718 <rahul.20223195@mnnit.ac.in>
2026-02-18 18:12:01 +0530

    added scheduler engine

---
+++ src/core/scheduler.ts
@@ -0,0 +1,41 @@
+import cron, { ScheduledTask } from "node-cron";
+import { log } from "./logger";
+
+type Job = {
+  name: string;
+  schedule: string;
+  task: () => Promise<void> | void;
+};
+// const jobs = new Map<string, cron.ScheduledTask>(); - this failed
+
+const jobs = new Map<string, ScheduledTask>();
+
+export function registerJob(job: Job) {
+  if (jobs.has(job.name)) {
+    log.error(`Job already registered: ${job.name}`);
+    return;
+  }
+
+  log.cron(`Registering job → ${job.name} (${job.schedule})`);
+
+  const scheduled = cron.schedule(job.schedule, async () => {
+    log.cron(`Running job → ${job.name}`);
+    try {
+      await job.task();
+      log.cron(`Completed job → ${job.name}`);
+    } catch (err: any) {
+      log.error(`Job failed → ${job.name}: ${err.message}`);
+    }
+  });
+
+  jobs.set(job.name, scheduled);
+}
+
+export function startScheduler() {
+  log.cron("Scheduler started");
+}
+
+export function stopScheduler() {
+  log.cron("Stopping scheduler...");
+  jobs.forEach((job) => job.stop());
+}
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,5 @@
+// npx ts-node-dev src/test.ts
+
 import { config } from "./core/env";
 // import { log } from "./core/logger";
 import { connectDB } from "./core/db";
@@ -8,6 +10,18 @@ connectDB();
 // log.boot("system starting");
 // log.db("connected");
 // log.error("something failed");
+
+import { registerJob, startScheduler } from "./core/scheduler";
+
+registerJob({
+  name: "testJob",
+  schedule: "*/1 * * * *",
+  task: async () => {
+    console.log("job running...");
+  },
+});
+
+startScheduler();
 
 
 console.log(config);
